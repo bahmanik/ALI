c5b4e1ef83d1b3b34d9b974e4abca3afe5d34751
bahmanik <bahmaniali350@gmail.com>
2026-05-14 02:43:16 +0330

    [F]ix dependency cycle in configuration

--- src/configuration/widgets/bar/modules/windowtitle/index.ts
+++ src/configuration/widgets/bar/modules/windowtitle/index.ts
@@ -1,28 +1,14 @@
-import { overrideVisualAsset } from "src/lib/options/factories/overrideVisualAsset"
 import { opt } from "src/lib/options"
-import type { BarCornerOptions } from "../../corner/type"
+import { windowtitleOptions } from "./type"
 
-const windowTitle: BarCornerOptions = {
-  enable: opt(true, { scss: true }),
-  gap: opt(0, { scss: true }),
-  edge: opt(0, { scss: true }),
-  radius: opt(12, { scss: true }),
-
-  background: overrideVisualAsset({
-    widgetId: "bar.corner",
-    defaultUseLocal: true,
-    defaultLocal: {
-      kind: "image",
-      path: "/home/ali/.config/ALI/background1",
-      technique: "none",
-    },
-    defaultRemote: ({ display }) => ({
-      kind: "image",
-      path: display.wallpaper.file.value,
-      technique: "none",
-    }),
-    deps: ["display.wallpaper.file"],
-  }),
+const windowTitle: windowtitleOptions = {
+  custom_title: opt<boolean>(true),
+  title_map: opt<Array<string>>([""]),
+  class_name: opt<boolean>(false),
+  label: opt<boolean>(true),
+  icon: opt<boolean>(true),
+  truncation: opt<boolean>(false),
+  truncation_size: opt<number>(10),
 }
 
 export default windowTitle 
--- src/configuration/widgets/bar/modules/windowtitle/type.ts
+++ src/configuration/widgets/bar/modules/windowtitle/type.ts
@@ -1,16 +1,11 @@
 import type { Opt } from "src/lib/options";
 
 export interface windowtitleOptions {
-  custom_title: Opt<true>,
+  custom_title: Opt<boolean>,
   title_map: Opt<Array<string>>,
   class_name: Opt<boolean>,
   label: Opt<boolean>,
   icon: Opt<boolean>,
   truncation: Opt<boolean>,
   truncation_size: Opt<number>,
-  leftClick: Opt<string>,
-  rightClick: Opt<string>,
-  middleClick: Opt<string>,
-  scrollUp: Opt<string>,
-  scrollDown: Opt<string>,
 }
